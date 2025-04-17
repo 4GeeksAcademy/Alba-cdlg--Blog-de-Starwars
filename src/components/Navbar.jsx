ddb8d352e9d4dbef426cf96763d6dfed6b825e6c
Alba-cdlg <alba.cdelagrana@icloud.com>
2025-04-17 09:27:52 +0000

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,17 @@
 const Navbar = () => {
-    return <div>Navbar</div>
+    return (
+        <nav className="navbar navbar-light bg-light">
+        <div className="container">
+            <a className="navbar-brand d-flex align-items-center" href="#">
+                <img
+                    src="https://logos-marcas.com/wp-content/uploads/2020/11/Star-Wars-Logo.png"
+                    alt="Star Wars Logo"
+                    className="logo-navbar me-2"
+                />
+            </a>
+        </div>
+    </nav>
+)
 }
 
 export default Navbar
