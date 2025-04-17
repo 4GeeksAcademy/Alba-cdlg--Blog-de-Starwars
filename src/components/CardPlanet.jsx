106c3867d0ad07dcb863f52f5920adb32cf05896
Alba-cdlg <alba.cdelagrana@icloud.com>
2025-04-17 10:22:00 +0000

    card planet

--- src/components/CardPlanet.jsx
+++ src/components/CardPlanet.jsx
@@ -1,7 +1,25 @@
-const CardPlanet = ({planet, uid}) => {
-    return (
-        <div className="card">
+const CardPlanet = ({ planet, uid }) => {
+  return (
+    <div className="card">
+      <img
+        src="https://via.placeholder.com/400x200"
+        className="card-img-top"
+        alt="{planet.name}"
+      />
+      <div className="card-body">
+        <h5 className="card-text">{planet.name}</h5>
+      </div>
+      <div className="d-flex justify-content-between">
+        <Link to={"/planet/${uid}"} className="btn btn-primary">
+          Más información
+        </Link>
+        <button className="">
+          <i className="bi bi-heart-fill"></i>
+        </button>
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
+
+export default CardPlanet
